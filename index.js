96b8b25489f52618b521bd8769d25548983b9dac
Nilesh <nileshnj993@gmail.com>
2022-01-31 13:41:08 +0530

    push final

--- index.js
+++ index.js
@@ -53,6 +53,7 @@ const userNotes = mongoose.Schema({
       date: String,
       expire: String,
       img: String,
+      url:String,
     },
   ],
 });
@@ -84,8 +85,12 @@ app.get("/home", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "home.html"));
 });
 
-app.post("/createAcc", (req, res) => {
-  console.log(req.body);
+app.post("/createAcc", async (req, res) => {
+  const user = await User.findOne({email:req.body.email});
+  if(user){
+      res.send('<html><head><title>Zeus Agent</title> <meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><link rel = "stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"><link rel = "stylesheet" href="css/styles.css"><link rel="icon" href="images/plane.png"></head><body><div class="card-body p-5 text-center" id="login"><img src="images/logo.png" width="10%" height="20%"></div><p style="font-weight: bolder;font-size: 300%;color:red;text-align: center; margin-top: -70px;">Zeus Agent</p><p style="font-weight: bolder;font-size: 200%;color:black;text-align: center; margin-top: 120px;">Email ID already Taken!</p><div id="successmsg" style="display: flex;justify-content: center;align-items: center;"><p style="font-size: 150%;color:black;"></p><a href="/signup"><input type="button" class="btn btn-outline-light btn-lg px-5" value="Create My Account" style="width:fit-content;font-weight: bolder; margin-top: 120px;"> </a></div></body></html>')
+  }
+  else{
   let plainPassword = req.body.password;
   bcrypt.hash(plainPassword, saltRounds, (err, hashedPassword) => {
     if (err) throw err;
@@ -99,20 +104,21 @@ app.post("/createAcc", (req, res) => {
       if (err) {
         res.status(500).send();
       } else {
-        res.sendFile(path.join(__dirname, "public", "registered.html"));
+        res.sendFile(path.join(__dirname, "/public", "registered.html"));
       }
     });
-    userNote = {
-      email: req.body.email,
-      desc: [],
-    };
-    stickyNote.create(userNote, (err, response) => {
-      if (err) {
-        res.status(500).send();
-        throw err;
-      }
-    });
+    var userNote = {
+        email: req.body.email,
+        desc: [],
+      };
+      stickyNote.create(userNote, (err, response) => {
+        if (err) {
+          res.status(500).send();
+          throw err;
+        }
+      });
   });
+}
 });
 
 app.post("/home", (req, res) => {
@@ -167,6 +173,7 @@ app.post("/createNote", (req, res) => {
       Public.create(note, (err, response) => {
         if (err) res.status(500).send();
         var id = response._id.toString();
+        note.url = process.env.ROUTE+id;
         stickyNote.updateOne({ email: req.cookies.userEmail }, { $push: { desc: note } }, (err, response) => {
           if (err) throw err;
           else {
@@ -182,7 +189,13 @@ app.get("/prevNotes", (req, res) => {
   stickyNote.find({ email: req.cookies.userEmail }, (err, response) => {
     if (err) throw err;
     else {
-      var time = new Date();
+      const temp = new Date();
+      var ISToffSet = 330; 
+      var offset= ISToffSet*60*1000;
+      var time = new Date(temp.getTime()+offset);
+
+      const expiretime = date.addSeconds(time, req.body.expirationTime);
+
       var data = response[0];
       var dataSend = {
         email: data.email,
@@ -228,7 +241,12 @@ app.get("/:id", (req,res) => {
     if(err) res.status(404).json("Incorrect URL");
     else {
       var data = response[0];
-      if(new Date() > new Date(data.expire))
+      console.log(data);
+      const temp = new Date();
+      var ISToffSet = 330; 
+      var offset= ISToffSet*60*1000;
+      var time = new Date(temp.getTime()+offset);
+      if(time > new Date(data.expire))
         res.status(404).json("Note Has Expired");
       res.render("publicNote", {data: data});
     }
